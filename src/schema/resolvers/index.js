43b9716bdfb91a180a451f539aea72666b546046
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-03-29 16:12:50 -0300

    Primeiro versão da arquitetura de arquivos

--- src/schema/resolvers/index.js
+++ src/schema/resolvers/index.js
@@ -1,20 +1,9 @@
-const fetch = require("node-fetch");
+const surveyResolver = require("./surveyResolver");
 
 // Objeto de resolvers
 const resolvers = {
   Query: {
-    survey: async () => {
-      try {
-        const response = await fetch(
-          "https://us-central1-serveless-survey-api.cloudfunctions.net/api/survey"
-        );
-        const data = await response.json();
-        return data;
-      } catch (error) {
-        console.error("Erro ao buscar pesquisa:", error);
-        throw error;
-      }
-    },
+    survey: surveyResolver,
   },
 };
 
