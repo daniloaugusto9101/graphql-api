8a73c47a785fc9f335702b7f1b4a3e00d0b5b0e8
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-04-02 21:29:35 -0300

    Update

--- src/schema/queries/surveyQuery.js
+++ src/schema/queries/surveyQuery.js
@@ -4,6 +4,16 @@ const surveyQuery = gql`
   type Query {
     survey: [Survey!]!
     getSurveyById(id: String!): Survey!
+    # delSurveyById(id: String!): Survey!
+    # postSurvey(
+    #   id: String!
+    #   title: String!
+    #   description: String
+    #   createdAt: String!
+    #   updatedAt: String
+    #   expiresAt: String!
+    #   options: [Option!]!
+    # ): Survey!
   }
 `;
 
