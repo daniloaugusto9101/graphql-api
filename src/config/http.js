61228bebd2a492d0197927102ab877def8951fc6
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-04-04 20:09:50 -0300

    Update

--- src/config/http.js
+++ src/config/http.js
@@ -4,11 +4,10 @@ const axios = require("axios");
 // const defaultEnv = "production"
 
 const api = axios.create({
-  //baseURL: "https://assem.com.br/wp-json/wp/v2/",
   baseURL: "https://us-central1-serveless-survey-api.cloudfunctions.net/api/",
   headers: {
     Authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbiI6ImFkbWluIiwiaWF0IjoxNzEyMTEwNDM4LCJleHAiOjE3MTIxMTQwMzh9.AsS8-lffvaVMf5vswt-c-wNoFiCa0AQca1QvAgHjgr0`,
-  }
+  },
 });
 
 module.exports = api;
